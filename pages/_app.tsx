424202e84f0d02f1918a1103bb8b4111af6232ca
Nicholas Charriere <nicholascharriere@gmail.com>
2023-04-03 20:56:18 -0700

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { PrivyProvider } from "@privy-io/react-auth";
+import { PrivyWagmiConnector } from "@privy-io/wagmi-connector";
+import { goerli, mainnet, configureChains } from "wagmi";
+import { infuraProvider } from "wagmi/providers/infura";
+
+const configureChainsConfig = configureChains(
+  [mainnet, goerli],
+  [
+    infuraProvider({
+      apiKey: process.env.NEXT_PUBLIC_INFURA_API_KEY as string,
+    }),
+  ]
+);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PrivyProvider appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID as string}>
+      <PrivyWagmiConnector wagmiChainsConfig={configureChainsConfig}>
+        <Component {...pageProps} />
+      </PrivyWagmiConnector>
+    </PrivyProvider>
+  );
 }
